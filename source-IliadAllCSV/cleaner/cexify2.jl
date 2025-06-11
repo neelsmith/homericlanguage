7c4af62e29776272fc7821fad21ff97413633c7f
Neel Smith <dnsmith.neel@gmail.com>
2025-06-11 07:07:55 -0400

    Extract and validate data

---
+++ source-IliadAllCSV/cleaner/cexify2.jl
@@ -0,0 +1,42 @@
+using CSV
+
+root = pwd()
+filesdir = joinpath(root, "source-IliadAllCSV", "cleaner")
+csvlist = filter(f -> endswith(f, "csv"), readdir(filesdir))
+
+
+
+formatted = ["URN|Text|Length|Word|Foot|Half-line"]
+
+for bk in 1:24
+    data = CSV.File(joinpath(filesdir, csvlist[bk]); drop = [7,8,9,10])    
+    @warn("Read these columns:")
+    @warn(data.names)
+    arr = collect(data)
+
+    currline = 0
+    for (i,r) in enumerate(arr)
+        urnval = "urn:cts:greekLit:tlg0012.tlg001.hmtx:$(bk).$(r[:Line])"
+        delimited = ""
+        syllabic = r[3]
+        textval = replace(r[2], r"[\.,;·]" => "")
+        if i == length(arr)
+            syllabic = "anceps"
+        elseif r[:Line] != arr[i + 1][:Line]
+            syllabic = "anceps"
+        end
+        wordidx = r[4]
+        footidx = r[5]
+        halfline = replace(r[6], "hemi" => "")
+        delimited = join([urnval, textval, syllabic, wordidx, footidx, halfline], "|")
+
+        
+        push!(formatted, delimited)
+    end
+    
+end
+
+
+open("formatted.cex", "w") do io
+    write(io, join(formatted,"\n"))
+end
