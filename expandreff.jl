9c58177992d6eb54413bdc378544a917fa31cbfb
Neel Smith <dnsmith.neel@gmail.com>
2025-06-11 21:34:51 -0400

    Expand references to get citable phenomena

---
+++ expandreff.jl
@@ -0,0 +1,50 @@
+f = "scansion.cex"
+data = readlines(f)[2:end]
+
+global currfoot = 0
+currsyll = 0
+currtoken = 0
+syllnum = 0
+
+
+delimitedoutput = ["Passage|Syllable|SyllableText|MetricLength|TokenIndex|Half-line"]
+for ln in data
+    syllurn = ""
+    cols = split(ln, "|")
+    psgurn = cols[1]
+    sylltext = cols[2]
+    sylllength = cols[3]
+    tknidx = cols[4]
+    halfline = cols[6]
+    
+
+    tokennum = cols[4]
+    if tokennum != currtoken
+        currtoken = tokennum
+    end
+
+    tokenurn = psgurn * "." * tokennum
+    footnum = cols[5]
+    if footnum != currfoot
+        
+        global currfoot = footnum
+        
+        syllnum = 1
+        #@info("Chnage foot to $(currfoot), syllnum to 1")
+        syllurn = join([psgurn,currfoot,syllnum],".")
+    else 
+
+        syllnum =  syllnum + 1
+        syllurn = join([psgurn,currfoot,syllnum],".")
+        #@warn("New syllurn " * syllurn)
+    end
+    #@warn(syllurn * ": " * sylltext * ", " * syllval)
+    delimitedline = join([psgurn,syllurn,sylltext,sylllength,tokenurn,halfline], "|")
+    push!(delimitedoutput, delimitedline)
+end
+
+open("scansion-expanded.cex","w") do io
+    write(io, join(delimitedoutput, "\n"))
+end
+
+# Passage|SyllableText|MetricLength|Token|Foot|Half-line
